cb1c06edd113ef37c3facac2d9c73bab9a9d5dd9
= <=>
2022-09-27 17:32:10 +0100

    .

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Header from './components/Header'
+
+function App() {
+
+  return (
+    <>
+      <div className="container">
+        <Header />
+      </div>
+    </>
+  )
+}
+
+export default App
